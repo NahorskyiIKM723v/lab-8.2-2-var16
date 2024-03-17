3e1165552642d094b82dc8424bd4f7e5f43ae8fe
igor <igor@DESKTOP-EH4IM96>
2024-03-18 00:50:59 +0200

    Добавьте файлы проекта.

---
+++ lab 8.2(2) var16/lab 8.2(2) var16.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_CTYPE, "ukr");
+    int arr[20];
+    cout << "Введіть 20 цілих чисел: ";
+    for (int i = 0; i < 20; i++) {
+        cin >> arr[i];
+    }
+        int reversedArr[20];
+    for (int i = 0; i < 20; i++) {
+        reversedArr[i] = arr[19 - i];
+    }
+    cout << "Зворотний масив: ";
+    for (int i = 0; i < 20; i++) {
+        cout << reversedArr[i] << " ";
+    }
+}
